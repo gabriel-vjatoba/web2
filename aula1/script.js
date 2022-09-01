3cd91142a721e6f7cd5e1f2771143b2c10cbe537
Aluno IFAL <alunoifal@IFAL.LOCAL>
2022-09-01 14:42:36 -0300

    fim da aula

---
+++ aula1/script.js
@@ -0,0 +1,16 @@
+// var em JS
+var home = "C:/home/" // var global
+let senha = "password" // var local
+
+const PI = 4.14159 // constante
+
+//String em JS
+let user = "Gabriel" // aspas duplas
+let user2 = 'João' // aspas simples
+let users = `${user}, ${user2}` // template string
+
+// Janels de diálogo no navegador
+alert(`Bem vindo, ${user}`)
+let idade = prompt("Qual a sua idade?")
+document.querySelector("h2").innerHTML("Sua idade é " + idade)
+let finish = confirm("Deseja concluir e sair?")
